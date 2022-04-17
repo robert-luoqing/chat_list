b68412c7a22bee2e2e0520d47ecffc820b47831c
robert-bitguild <54098389+robert-bitguild@users.noreply.github.com>
2022-04-17 14:00:04 +0800

    Update msg_provider.dart

--- example/lib/msg_provider.dart
+++ example/lib/msg_provider.dart
@@ -96,4 +96,65 @@ class MsgProvider {
       return msgObj;
     }
   }
+
+  /// 1. Add [addedMsgs] into [messages] and remove duplicated messages
+  /// 2. Generate time tag amount [addedMsgs] (keep time tag in messages not to change)
+  /// if [timeTagPolicies] is ["5m", "20m", "1h", "3h"], is mean create a tag in 5min, 20min, 1hour and 3 hour,
+  /// after that 3hours, create a time tag each 3 hours
+  List<T> mergeIncomeNewMessages<T>(
+      {required List<T> messages,
+      required bool isAddTop,
+      required List<T> addedMsgs,
+      required DateTime Function(T) getMessageTime,
+      required bool Function() isTimeTagMsg,
+      required T Function(DateTime time) onCreateTimeTag,
+      required List<String> timeTagPolicies,
+      bool needRegenerateTimeTag = false}) {
+    return messages;
+  }
+
+  /// Generate time tag amount [messages]
+  /// if [timeTagPolicies] is ["5m", "20m", "1h", "3h"], is mean create a tag in 5min, 20min, 1hour and 3 hour,
+  /// after that 3hours, create a time tag each 3 hours
+  List<T> generateTimeTags<T>({
+    required List<T> messages,
+    required DateTime Function(T) getMessageTime,
+    required T Function(DateTime time) onCreateTimeTag,
+    required List<String> timeTagPolicies
+  }) {
+    List<T> result = <T>[];
+    var policies = parseTimeTagPolicies(timeTagPolicies);
+    if (policies.isEmpty) {
+      result = messages;
+    } else {
+      int curPolIndex = 0;
+      int curPolicy = policies[
+          curPolIndex >= policies.length ? policies.length : curPolIndex];
+      for (var msg in messages) {
+        var msgTime = getMessageTime(msg);
+      }
+    }
+
+    return result;
+  }
+
+  List<int> parseTimeTagPolicies(List<String> timeTagPolicies) {
+    var result = <int>[];
+    for (var policy in timeTagPolicies) {
+      var endStr = policy.substring(policy.length - 1);
+      var prefixNum = policy.substring(0, policy.length - 1);
+      if (endStr == "s") {
+        result.add(int.parse(prefixNum) * 1000);
+      } else if (endStr == "m") {
+        result.add(int.parse(prefixNum) * 1000 * 60);
+      } else if (endStr == "h") {
+        result.add(int.parse(prefixNum) * 1000 * 60 * 60);
+      } else if (endStr == "d") {
+        result.add(int.parse(prefixNum) * 1000 * 60 * 60 * 24);
+      }
+    }
+
+    result.sort((a, b) => a - b);
+    return result;
+  }
 }
